45bcbea35f609183a86b38190454bd534d658a8d
Shraddha <jadhavshradddha5@gmail.com>
2023-09-13 22:43:46 +0530

    Password validation

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -24,11 +24,25 @@ public class LoginServlet extends HttpServlet {
         }
     }*/
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  /*  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String user = req.getParameter("username");
         String pwd = req.getParameter("pass");
        // boolean b = user.matches("[A-Z][a-zA-Z]*{3,}");
         if (user.matches("[A-Z][a-zA-Z]*.{2,}") && pwd.equals("Password")) {
+            RequestDispatcher rd = req.getRequestDispatcher("LoginSuccessfully.html");
+            rd.forward(req, resp);
+        } else {
+            resp.setContentType("text/html");
+            PrintWriter out = resp.getWriter();
+            out.println("<font color=red>Either user name or password is wrong.</font>");
+            out.close();
+        }
+    }*/
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String user = req.getParameter("username");
+        String pwd = req.getParameter("pass");
+        // boolean b = user.matches("[A-Z][a-zA-Z]*{3,}");
+        if (user.matches("Admin") && pwd.equals("^[a-z](?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}$")) {
             RequestDispatcher rd = req.getRequestDispatcher("LoginSuccessfully.html");
             rd.forward(req, resp);
         } else {
