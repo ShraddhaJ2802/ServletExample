09205f7b178add092d8e478540b82fdd378c317b
Shraddha <jadhavshradddha5@gmail.com>
2023-09-13 22:15:42 +0530

    Name starts with Cap and has minimum 3 character

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -10,10 +10,25 @@ import java.io.PrintWriter;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+   /* protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String user = req.getParameter("username");
         String pwd = req.getParameter("pass");
         if (user.equals("Admin") && pwd.equals("Password")) {
+            RequestDispatcher rd = req.getRequestDispatcher("LoginSuccessfully.html");
+            rd.forward(req, resp);
+        } else {
+            resp.setContentType("text/html");
+            PrintWriter out = resp.getWriter();
+            out.println("<font color=red>Either user name or password is wrong.</font>");
+            out.close();
+        }
+    }*/
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String user = req.getParameter("username");
+        String pwd = req.getParameter("pass");
+       // boolean b = user.matches("[A-Z][a-zA-Z]*{3,}");
+        if (user.matches("[A-Z][a-zA-Z]*.{2,}") && pwd.equals("Password")) {
             RequestDispatcher rd = req.getRequestDispatcher("LoginSuccessfully.html");
             rd.forward(req, resp);
         } else {
